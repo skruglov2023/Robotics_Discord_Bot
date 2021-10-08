fcdb5a06a97aad0d01890ed8ba151431fbd35842
skruglov2023 <skruglov.2023@carmelhs.org>
2021-10-07 20:54:11 -0500

    First commit; bot lives

--- Robotics_Discord_Bot.py
+++ Robotics_Discord_Bot.py
@@ -1,14 +1,21 @@
+import discord
+from discord.ext import commands
 TokenPath = '/home/pi/Desktop/scripts/Robotics_token'
-#GuildPath='/home/pi/Desktop/scripts/guild'
-
-#with open(GuildPath, 'r') as guil:
-#    global GUILD
-#    almostGuild = guil.read()
-#    GUILD =  almostGuild
+GuildPath='/home/pi/Desktop/scripts/Robotics_guild'
+#load files using above paths
+with open(GuildPath, 'r') as guil:
+    global GUILD
+    almostGuild = guil.read()
+    GUILD =  almostGuild
 with open(TokenPath, 'r') as toke:
     global TOKEN
     almostToken = toke.read()
     TOKEN =  almostToken
-bot = commands.Bot(command_prefix='!')
 
-bot.run(TOKEN)
+bot = commands.Bot(command_prefix='!') #give bot some usage parameters
+@bot.event
+async def on_ready():
+    print("Logged in as Robotics Bot")
+
+
+bot.run(TOKEN) #run bot
